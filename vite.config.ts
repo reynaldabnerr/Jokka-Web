c43c3183685e99ffe3c341700fea0580daf447b4
reynaldabnerr <reynald030685@gmail.com>
2024-12-09 20:02:14 +0800

    hosting with firebase

--- vite.config.ts
+++ vite.config.ts
@@ -1,19 +1,13 @@
 /// <reference types="vitest" />
 
-import legacy from '@vitejs/plugin-legacy'
-import react from '@vitejs/plugin-react'
-import { defineConfig } from 'vite'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  base: '/Jokka-Web/',
-  plugins: [
-    react(),
-    legacy()
-  ],
-  test: {
-    globals: true,
-    environment: 'jsdom',
-    setupFiles: './src/setupTests.ts',
-  }
-})
+  plugins: [react()],
+  build: {
+    outDir: "dist",
+    assetsDir: "assets",
+  },
+  publicDir: "public",
+});
